f8514213389c9c37dc085c4a289cb3b182a64358
Illia Karpoff <hawaii2001@ukr.net>
2020-07-12 11:30:00 +0300

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,89 @@
+// Завдання 1
+// Напиши функцію logItems(array), яка отримує масив і використовує цикл for, який для кожного елемента
+//  масиву буде виводити в консоль повідомлення в форматі [номер елемента] - [значення елемента].
+
+// Нумерація повинна починатися з 1. Наприклад, для першого елемента масиву ['Mango', 'Poly', 'Ajax'] 
+// з індексом 0 буде виведено '1 - Mango', а для індексу 2 виведе '3 - Ajax'.
+
+const logItems = function (array) {
+
+    // for (const argument of arguments) {
+    // }
+    array = Array.from(arguments);
+    console.log(array);
+ 
+    for (let i = 0; i < array.length; i += 1) {
+        console.log(`${i+1} - ${array[i]}`);
+    };
+};
+
+// logItems('Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong');
+logItems(5, 10, 15, 20, 25, 30, 35, 40, 45, 50);
+
+// const logItems = function(array) {
+    
+    
+//     for ( let i = 0; i < array.length; i += 1) {
+//         console.log(`${i+1} - ${logItems[i]}`);
+        
+//     };
+// }
+
+
+
+// const logItems = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+
+/*
+const logItems   = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+console.log(logItems);
+for (let i = 0; i < logItems.length; i += 1) {
+        // logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+        console.log(`${i+1} - ${logItems[i]}`);
+    };
+*/
+
+
+// const cats = ['Mango', 'Poly', 'Ajax'];
+// const catNameToFind = prompt('введи имя кота');
+// let message;
+
+// for (const cat of cats) {
+//   /*
+//    * На каждой итерации мы будем проверять совпадает ли элемент массива с
+//    * имеprompt('введи имя кота'клиента. Если совпадает то мы записываем в message сообщение
+//    * об успехе и делаем break чтобы не искать дальше
+//    */
+
+//   if (cat === catNameToFind) {
+//     message = 'Кит с таким именем есть в базе данных!';
+//     break;
+//   }
+
+//   // Если они не совпадают то запишем в resultMsg сообщeние об отсутсвии имени
+//   message = 'Кита с таким именем нету в базе данных!';
+// }
+
+// alert(message); // Клиент с таким именем есть в базе данных!
+
+
+
+// const matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ];
+//   let total = 0;
+  
+//   for (let i = 0; i < matrix.length; i += 1) {
+//     console.log('Подмассив матрицы matrix[i]: ', matrix[i]);
+  
+//     for (let j = 0; j < matrix[i].length; j += 1) {
+//       console.log('Элемент подмассива матрицы matrix[i][j]: ', matrix[i][j]);
+//       total += matrix[i][j];
+//     }
+//   }
+  
+//   console.log('Summary', total); // 45
+  
+
+  
---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+Завдання 3
+Напиши функцію findLongestWord(string), яка приймає параметром довільний рядок (в рядку будуть тільки слова і прогалини) і повертає найдовше слово в цьому рядку.
+
+const findLongestWord = function (string) {
+  // твій код
+};
+
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
