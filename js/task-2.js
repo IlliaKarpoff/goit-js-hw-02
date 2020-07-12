c9cda19f66ba33ab1c1670518a66dfa1b5b8d0e8
Illia Karpoff <hawaii2001@ukr.net>
2020-07-12 12:32:05 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -4,30 +4,16 @@
 //  і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
 
 const calculateEngravingPrice = function (message, pricePerWord) {
-  // твій код
+  const array = message.split(' ');
+  return array.length * pricePerWord;
 };
 
-/*
- * Виклич функції для перевірки працездатності твоєї реалізації.
- */
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    10,
-  ),
-); // 80
+// Виклич функції для перевірки працездатності твоєї реалізації.
 
-console.log(
-  calculateEngravingPrice(
-    'Proin sociis natoque et magnis parturient montes mus',
-    20,
-  ),
-); // 160
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
 
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-); // 240
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
 
-console.log(
-  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-); // 120
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
